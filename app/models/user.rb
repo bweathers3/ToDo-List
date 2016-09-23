4b2ac54387463180f9fc32abeccce9d30a535055
Bailey <bailey.g.weathers@gmail.com>
2016-09-23 16:23:55 -0500

    Complete Destory Users and Lists

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,4 @@ class User < ActiveRecord::Base
 
   has_many :lists, dependent: :destroy
 
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,8 @@ root 'welcome#index'
   namespace :api, defaults: { format: :json } do
 
    resources :users do
-      resources :lists, only: [:create]
+      resources :lists, only: [:create, :destroy]
    end
-
-   #resources :lists
 
     resources :lists, only: [] do
       resources :items, only: [:create]
